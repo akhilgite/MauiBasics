d7cd445ead4358c2e987108b5c3cffb31bea125c
akhilgite <GITEAKH@HILTI.com>
2023-11-04 21:29:21 +0530

    first commit

---
+++ MauiBasics/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBasics;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiBasics/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBasics;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
